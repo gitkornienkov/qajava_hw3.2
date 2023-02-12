eeadf0980cd56677ea63b783148458b1a818edbb
pavelkornienkov <pikornienkov@gmail.com>
2023-02-12 23:45:22 +0300

    Added Math.pow(height, 2) Deleted heightDegree = height * height

--- src/BmiService.java
+++ src/BmiService.java
@@ -2,8 +2,8 @@ public class BmiService {
 
     public int bmiCalc(double height, double weight) {
         double bmiCalculate;
-        double heightDegree = height * height;
-        bmiCalculate = weight / heightDegree;
+        //double heightDegree = height * height;
+        bmiCalculate = weight / Math.pow(height, 2);
         return (int)bmiCalculate;
     }
 }
